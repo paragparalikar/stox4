cdf112fad8b8f306cde98cbf58590754649f8633
Parag Paralikar <parag.paralikar@gmail.com>
2022-01-26 16:08:57 +0530

    na

--- stox4j/src/main/java/com/stox/StoxApplication.java
+++ stox4j/src/main/java/com/stox/StoxApplication.java
@@ -3,11 +3,27 @@ package com.stox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dlsc.workbenchfx.Workbench;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 @SpringBootApplication
-public class StoxApplication {
+public class StoxApplication extends Application {
 
 	public static void main(String[] args) {
+		launch(args);
 		SpringApplication.run(StoxApplication.class, args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		final Workbench workbench = Workbench.builder().build();
+		final Scene myScene = new Scene(workbench);
+	    primaryStage.setScene(myScene);
+	    primaryStage.setMaximized(true);
+	    primaryStage.show();
 	}
 	
 }
