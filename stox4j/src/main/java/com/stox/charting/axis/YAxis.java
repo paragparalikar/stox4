b7611a14ddb42fce4862bee8e3d06701b2281474
Parag Paralikar <parag.paralikar@gmail.com>
2022-02-19 15:42:08 +0530

    na

--- stox4j/src/main/java/com/stox/charting/axis/YAxis.java
+++ stox4j/src/main/java/com/stox/charting/axis/YAxis.java
@@ -5,20 +5,14 @@ import com.stox.charting.ChartingView.ChartingContext;
 import com.stox.charting.grid.Crosshair;
 import com.stox.charting.grid.HorizontalGrid;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+@SuppressWarnings("unused")
 public class YAxis extends StackPane {
-	public static final double WIDTH = 64;
-	private static final Insets PADDING = new Insets(0, 4, 0, 0);
-	private static final Font FONT = Font.font(12);
 	private static final double[] TICK_UNIT_DEFAULTS = { 1.0E-10d, 2.5E-10d, 5.0E-10d, 1.0E-9d, 2.5E-9d, 5.0E-9d,
 			1.0E-8d, 2.5E-8d, 5.0E-8d, 1.0E-7d, 2.5E-7d, 5.0E-7d, 1.0E-6d, 2.5E-6d, 5.0E-6d, 1.0E-5d, 2.5E-5d, 5.0E-5d,
 			1.0E-4d, 2.5E-4d, 5.0E-4d, 0.0010d, 0.0025d, 0.0050d, 0.01d, 0.025d, 0.05d, 0.1d, 0.25d, 0.5d, 1.0d, 2.5d,
@@ -27,7 +21,10 @@ public class YAxis extends StackPane {
 			5.0E8d, 1.0E9d, 2.5E9d, 5.0E9d, 1.0E10d, 2.5E10d, 5.0E10d, 1.0E11d, 2.5E11d, 5.0E11d, 1.0E12d, 2.5E12d,
 			5.0E12d };
 	
-	private Pane container = new Pane();
+	private final Crosshair crosshair;
+	private final ChartingConfig config;
+	private final ChartingContext context;
+	private final Pane container = new Pane();
 	private final HorizontalGrid horizontalGrid;
 	@Setter @Getter private double 
 			highestValue = Double.MIN_VALUE, 
@@ -36,12 +33,17 @@ public class YAxis extends StackPane {
 	
 	public YAxis(ChartingContext context, ChartingConfig config, 
 			Crosshair crosshair, HorizontalGrid horizontalGrid) {
-		setWidth(WIDTH);
-		setMaxWidth(WIDTH);
-		setMinWidth(WIDTH);
-		setPrefWidth(WIDTH);
-		getChildren().add(container);
+		style();
+		this.config = config;
+		this.context = context;
+		this.crosshair = crosshair;
 		this.horizontalGrid = horizontalGrid;
+		getChildren().add(container);
+	}
+	
+	private void style() {
+		getStyleClass().add("y-axis");
+		container.getStyleClass().add("container");
 	}
 	
 	public void reset() {
@@ -72,14 +74,10 @@ public class YAxis extends StackPane {
 	
 	private void addLabel(double value, double labelHeight) {
 		final Label label = new Label(String.format("%.2f", value));
-		label.setFont(FONT);
-		label.setPadding(PADDING);
-		label.setAlignment(Pos.CENTER_RIGHT);
-		
 		final double y = getY(value);
 		horizontalGrid.addLine(y);
-		label.resizeRelocate(0, y - labelHeight/2, WIDTH, labelHeight);
 		container.getChildren().add(label);
+		label.layoutYProperty().bind(label.heightProperty().divide(2).subtract(y).negate());
 	}
 	
 	private double snap(double value) {
